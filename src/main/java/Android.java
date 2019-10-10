61481219ae10c8a888a1d26363483c7a5a499fe8
IYAN OLIVER ALVAREZ (PLAN-IRIS-ISD-OOCLL/MNL) <ALVARIY@OOCL.COM>
2019-10-10 12:15:42 +0800

    Add Mobile Class

---
+++ src/main/java/Android.java
@@ -0,0 +1,8 @@
+public class Android extends Mobile {
+
+    public void call(String message)
+    {
+        System.out.println("<Android>Message :" + message);
+    }
+
+}
---
+++ src/main/java/Iphone.java
@@ -0,0 +1,9 @@
+public class Iphone extends Mobile{
+
+    public void call(String message)
+    {
+        System.out.println("<Iphone>Message :" + message);
+    }
+
+
+}
